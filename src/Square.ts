8820bf3f54dff13f9692080d8f06a4fde887b0be
Thiago Sá <thiagottsa@gmail.com>
2015-05-19 21:07:39 -0300

    Clock's got colors.

--- src/Square.ts
+++ src/Square.ts
@@ -2,7 +2,7 @@
 
 class Square {
 	public state: SquareState;
-	
+
 	constructor(public xAxis: number, public yAxis: number, public length: number) {
 		this.state = SquareState.Off;
 	}
@@ -10,10 +10,26 @@ class Square {
 	public draw(context: CanvasRenderingContext2D): void {
 		context.beginPath();
 		context.rect(this.xAxis, this.yAxis, this.length, this.length);
-		context.fillStyle = 'grey';
+		context.fillStyle = this.getColor();
 		context.fill();
 		context.lineWidth = 1;
 		context.strokeStyle = 'black';
 		context.stroke();
+	}
+
+	private getColor(): string {
+		switch (this.state) {
+			case SquareState.Off:
+				return 'grey';
+
+			case SquareState.Blue:
+				return 'blue';
+
+			case SquareState.Red:
+				return 'red';
+
+			case SquareState.Green:
+				return 'green';
+		}
 	}
 }
