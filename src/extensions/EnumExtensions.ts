be83f0c27c003d7ec83152526c0966de86a97503
Thiago <thiagottsa@gmail.com>
2020-02-16 15:23:33 +0000

    Fix TS compilation.

--- src/extensions/EnumExtensions.ts
+++ src/extensions/EnumExtensions.ts
@@ -2,7 +2,7 @@ class EnumExtensions {
     static getValues(e: any): Array<number> {
         var a: Array<number> = [];
         for (var val in e) {
-            if (!isNaN(val)) {
+            if (!isNaN(Number(val))) {
                 a.push(parseInt(val, 10));
             }
         }
